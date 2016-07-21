f3ae90981803cf24e9376246655c023e9f4d0c0b
Adit Chandra <aditchandra@college.harvard.edu>
2016-07-20 22:49:48 -0700

    loggin fuzzy match

--- libs/movieProvider.js
+++ libs/movieProvider.js
@@ -25,6 +25,7 @@ function removeLeadingArticles(title) {
 function fuzzyMatch (title) {
   // get top-scored fuzzy match title property
   var matches = fuse.search(removeLeadingArticles(title));
+  console.log(matches[0]);
   return matches[0];
 }
 
