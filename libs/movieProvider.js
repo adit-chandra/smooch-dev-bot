512e84b03c994016f7e729f70b1702072600b0c9
Adit Chandra <aditchandra@college.harvard.edu>
2016-07-20 22:49:05 -0700

    muting console

--- libs/movieProvider.js
+++ libs/movieProvider.js
@@ -10,10 +10,10 @@ fs.createReadStream('moviemap.csv')
     .pipe(csv())
     .on('data', function(data) {
         movie_dictionary.push(data.Movie);
-        console.log('adding: ' + data.Movie);
+        // console.log('adding: ' + data.Movie);
     })
     .on('end', function(){
-        console.log(movie_dictionary);
+        // console.log(movie_dictionary);
     });
 
 var fuse = new Fuse(movie_dictionary);
